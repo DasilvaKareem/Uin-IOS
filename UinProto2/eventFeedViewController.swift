de14e7e2a0effd44f21509fb99442f4dc2be6114
coleproe <croe@areuin.co>
2015-02-01 15:16:02 -0600

    added ui to create event view

--- UinProto2/eventFeedViewController.swift
+++ UinProto2/eventFeedViewController.swift
@@ -37,6 +37,8 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
+        
         navigationController?.navigationBar.barTintColor = UIColor(red:60.0/255.0, green:144.0/255.0,blue:201.0/250.0,alpha:1.0)
         
         
