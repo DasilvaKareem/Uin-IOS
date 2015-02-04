fb5a070c75081b116c7fc1fd379b33df4d48db2b
Kareem Dasilva <kdasilva@memphis.edu>
2015-02-03 23:21:35 -0600

    Sanitize some code

--- UinProto2/postEvent.swift
+++ UinProto2/postEvent.swift
@@ -9,57 +9,31 @@
 import UIKit
 
 class postEvent: UIViewController {
-
- 
     
     @IBOutlet weak var eventTitle: UILabel!
-    
     @IBOutlet weak var location: UILabel!
-    
     @IBOutlet weak var time: UILabel!
-    
     @IBOutlet weak var date: UILabel!
- 
     @IBOutlet weak var isFood: UIImageView!
-    
+    @IBOutlet weak var username: UIButton!
+    @IBOutlet weak var isSite: UIImageView!
+    @IBOutlet weak var isPaid: UIImageView!
     @IBAction func gotoProfile(sender: AnyObject) {
         
         self.performSegueWithIdentifier("gotoprofile", sender: self)
         
     }
     
-    
-    
-    @IBOutlet weak var username: UIButton!
-   
-
-    
-    @IBOutlet weak var isSite: UIImageView!
-    
-    
- 
-    
-    @IBOutlet weak var isPaid: UIImageView!
-  
     var users = String()
-    
     var storeTitle = String()
-    
     var storeLocation = String()
-    
     var storeTime = String()
-    
     var storeDate = String()
-    
     var storeSum = String()
-    
     var data = Int()
-    
     var onsite = Bool()
-    
     var food = Bool()
-    
-    var  cost = Bool()
+    var cost = Bool()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,110 +41,45 @@ class postEvent: UIViewController {
        username.setTitle(users, forState: UIControlState.Normal)
         
        location.text = storeLocation
-        
         eventTitle.text = storeTitle
-        
-       
-        
         time.text = storeTime
-        
         date.text = storeDate
         putIcons()
-        
-    
-        //self.example.text = self.data
-        // Do any additional setup after loading the view.
+  
     }
-    
-   
     
     func putIcons(){
         
         
         if  onsite == true {
-            
-            
             isSite.image = UIImage(named: "oncampusicon@3x.png")
-         
-            
-            
-            
         }
         else{
-            
             isSite.image = UIImage(named: "offCampus@3x.png")
-            
-   
-            
         }
-        
+     
         if food == true {
-            
-            
             isFood.image = UIImage(named: "foodicon@3x.png")
-           
-            
-            
         }
         else{
-            
             isFood.image = UIImage(named: "noFood@3x.png")
-            
-        
-            
-            
         }
         if cost == false {
-            
-            
-            isPaid.image = UIImage(named: "freeicon@3x.png")
-           
-            
-            
+            isPaid.image = UIImage(named: "freeicon@3x.png"
         }
         else{
-            
             isPaid.image = UIImage(named: "noFree@3x.png")
-            
-            
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
+       }
     }
     override func prepareForSegue(segue:UIStoryboardSegue, sender: AnyObject?){
-        
-        
-        
+ 
         if segue.identifier == "gotoprofile" {
             var theotherprofile:userprofile = segue.destinationViewController as userprofile
-            
-            
             theotherprofile.theUser = users
-            
-            
-            
-            
-            
-            
-        }
-        
-        
-        
+      }
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-
-
-
+   }
 }
