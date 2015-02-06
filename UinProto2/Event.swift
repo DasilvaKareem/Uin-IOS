d0bc966a5af09b8fb6593a1098cdc52f2f2a964d
Ismael Alonso <ismaha91@gmail.com>
2015-02-06 11:02:41 -0600

    Added the Event class that shall be used as the type holder in the event feed view controller

---
+++ UinProto2/Event.swift
@@ -0,0 +1,120 @@
+//
+//  Event.swift
+//  UinProto2
+//
+//  Created by Ismael Alonso on 2/6/15.
+//  Copyright (c) 2015 Kareem Dasilva. All rights reserved.
+//
+
+import Foundation
+
+public class Event{
+    //Event attributes
+    private var onCampus: Bool
+    private var paid: Bool
+    private var food: Bool
+    private var title: String
+    private var location: String
+    private var startDate: String
+    private var endDate: String
+    private var startTime: String
+    private var endTime: String
+    private var author: String
+    
+    
+    //Initialiser
+    init(){
+        onCampus = false
+        paid = false
+        food = false
+        title = ""
+        location = ""
+        startDate = ""
+        endDate = ""
+        startTime = ""
+        endTime = ""
+        author = ""
+    }
+    
+    //Setters
+    func setOnCampus(onCampus: Bool){
+        self.onCampus = onCampus
+    }
+    
+    func setPaid(paid: Bool){
+        self.paid = paid
+    }
+    
+    func setFood(food: Bool){
+        self.food = food
+    }
+    
+    func setTitle(title: String){
+        self.title = title
+    }
+    
+    func setLocation(location: String){
+        self.location = location
+    }
+    
+    func setStartDate(startDate: String){
+        self.startDate = startDate
+    }
+    
+    func setEndDate(endDate: String){
+        self.endDate = endDate
+    }
+    
+    func setStartTime(startTime: String){
+        self.startTime = startTime
+    }
+    
+    func setEndTime(endTime: String){
+        self.endTime = endTime
+    }
+    
+    func setAuthor(author: String){
+        self.author = author
+    }
+    
+    //Getters
+    public func getOnCampus() -> Bool{
+        return onCampus
+    }
+    
+    public func getPaid() -> Bool{
+        return paid
+    }
+    
+    public func getFood() -> Bool{
+        return food
+    }
+    
+    public func getTitle() -> String{
+        return title
+    }
+    
+    public func getLocation() -> String{
+        return location
+    }
+    
+    public func getStartDate() -> String{
+        return startDate
+    }
+    
+    public func getEndDate() -> String{
+        return endDate
+    }
+    
+    public func getStartTime() -> String{
+        return startTime
+    }
+    
+    public func getEndTime() -> String{
+        return endTime
+    }
+    
+    public func getAuthor() -> String{
+        return author
+    }
+}
