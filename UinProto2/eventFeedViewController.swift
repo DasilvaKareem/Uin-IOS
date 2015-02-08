995ac53343ea306ac45b1a5cbe6b06746a428611
Kareem Dasilva <kareemdasilva@crewsmac01.memphis.edu>
2015-02-08 15:50:13 -0600

    Stuff

--- UinProto2/eventFeedViewController.swift
+++ UinProto2/eventFeedViewController.swift
@@ -25,6 +25,7 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
     var eventStartDate = [String]()
     var eventEndDate = [String]()
     var usernames = [String]()
+    var objectID = [String]()
     
    
     
@@ -61,6 +62,8 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
        
      }
      func updateFeed(){
+        //Removes all leftover content in the array
+        self.objectID.removeAll(keepCapacity: true)
         self.usernames.removeAll(keepCapacity: true)
         self.eventTitle.removeAll(keepCapacity: true)
         self.eventlocation.removeAll(keepCapacity: true)
@@ -73,6 +76,7 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
         self.food.removeAll(keepCapacity: true)
         self.onsite.removeAll(keepCapacity: true)
         
+        //adds content to the array
       var que = PFQuery(className: "event")
         que.orderByAscending("dateTime")
         que.whereKey("public", equalTo: true)
@@ -87,7 +91,8 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
                 
                 for object in objects{
                     
-                    println(object.objectId)
+                    
+                    self.objectID.append(object.objectId as String)
                     self.usernames.append(object["user"] as String)
                     self.eventTitle.append(object["sum"] as String)
                     self.eventStartDate.append(object["startdate"] as String)
@@ -121,11 +126,13 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
                 for object in objects{
                     
                     println(object.objectId)
+                    self.objectID.append(object.objectId as String)
                     self.usernames.append(object["user"] as String)
-               
                     self.eventTitle.append(object["sum"] as String)
+                    self.eventStartDate.append(object["startdate"] as String)
                     self.eventEndDate.append(object["endDate"] as String)
                     self.eventStartTime.append(object["starttime"] as String)
+                    self.eventEndTime.append(object["endTime"] as String)
                     self.food.append(object["food"] as Bool)
                     self.paid.append(object["paid"] as Bool)
                     self.onsite.append(object["location"] as Bool)
@@ -190,7 +197,7 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
             cell.onCampusIcon.image = UIImage(named: "onCampus.png")
             cell.onCampusText.text = "On-Campus"
             cell.onCampusText.textColor = UIColor.darkGrayColor()
-            println("yo")
+           
             
         }
         else{
@@ -206,7 +213,7 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
             cell.foodIcon.image = UIImage(named: "yesFood.png")
             cell.foodText.text = "Food"
             cell.foodText.textColor = UIColor.darkGrayColor()
-            println("yo")
+           
             
         }
         else{
@@ -221,7 +228,7 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
             cell.freeIcon.image = UIImage(named: "yesFree.png")
             cell.costText.text = "Free"
             cell.costText.textColor = UIColor.darkGrayColor()
-            println("yo")
+          
             
         }
         else{
@@ -236,20 +243,62 @@ class eventFeedViewController: UIViewController, UITableViewDelegate, UITableVie
         cell.time.text = eventStartTime[indexPath.row]
         cell.eventName.text = eventlocation[indexPath.row]
         cell.poop.tag = indexPath.row
-       cell.poop.addTarget(self, action: "followButton:", forControlEvents: UIControlEvents.TouchUpInside)
+        // Mini query to check if event is already saved
+        println(objectID[indexPath.row])
+        var minique = PFQuery(className: "GoingEvent")
+        minique.whereKey("user", equalTo: PFUser.currentUser().username)
+        var minique2 = PFQuery(className: "GoingEvent")
+        minique.whereKey("eventID", equalTo: objectID[indexPath.row])
+       // minique2.whereKey("eventID", matchesKey:objectID[indexPath.row] , inQuery:minique )
+        minique.getFirstObjectInBackgroundWithBlock{
+            
+            (results:PFObject!, error: NSError!) -> Void in
+            
+            if error == nil {
+                
+                cell.poop.setImage(UIImage(named: "addedToCalendar.png"), forState: UIControlState.Normal)
+                
+            }   else {
+                
+                
+                cell.poop.setImage(UIImage(named: "addToCalendar.png"), forState: UIControlState.Normal)
+            }
+            
+            
+        }
+        cell.poop.addTarget(self, action: "followButton:", forControlEvents: UIControlEvents.TouchUpInside)
   
         return cell
     }
     
     func followButton(sender: AnyObject){
         // Puts the data in a cell
-        var que = PFQuery(className: "events")
+        
+   
         var eventStore : EKEventStore = EKEventStore()
         eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion: {
+            
             granted, error in
             if (granted) && (error == nil) {
                 println("granted \(granted)")
                 println("error  \(error)")
+                var going = PFObject(className: "GoingEvent")
+                going["user"] = PFUser.currentUser().username
+                going["event"] = self.eventTitle[sender.tag]
+                going["author"] = self.usernames[sender.tag]
+                going["added"] = true
+                going["eventID"] = self.objectID[sender.tag]
+                going.saveInBackgroundWithBlock{
+                    
+                    (succeded:Bool!, savError:NSError!) -> Void in
+                    
+                    if savError == nil {
+                        
+                        println("it worked")
+                    
+                    }
+                }
+                
                 
                 var event:EKEvent = EKEvent(eventStore: eventStore)
                 event.title = self.eventlocation[sender.tag]
