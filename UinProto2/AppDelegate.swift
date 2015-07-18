fb6a528fe859920b6923ae6e7b34811d5f695149
Kareem Dasilva <kareemdasilva@Kareems-MacBook-Pro.local>
2015-07-18 12:35:38 -0500

    added Facebook login

--- UinProto2/AppDelegate.swift
+++ UinProto2/AppDelegate.swift
@@ -36,10 +36,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         ParseCrashReporting.enable()
         //devolpment key
-        //Parse.setApplicationId("HawWPTDabdo1FXdxMevotUNmVCwVl62wTkjiFyNg", clientKey: "Gj2fnhH0CkA6G1Hkl5igSQRooXeD3ByVBKjMF1Bq")
+        Parse.setApplicationId("0ShUBAdjISFynDKChHvUdDaL5wcMUke0NTbY6myP", clientKey: "3dXP6g3Em7gfIIbsrBWog1yDLzhMuzonjp6u5spJ")
         
         //Production Key
-         Parse.setApplicationId("BFxrzfMk4LK2WDbBdwtfeWmFcZwZwkMLdryiDPwm", clientKey: "tALwULorTkQbcVv3JHqVtTDrrelIZFSebtb0cHJs")
+         //Parse.setApplicationId("BFxrzfMk4LK2WDbBdwtfeWmFcZwZwkMLdryiDPwm", clientKey: "tALwULorTkQbcVv3JHqVtTDrrelIZFSebtb0cHJs")
         
         //Set the user Time stamp
   
--- UinProto2/registrationProcess.swift
+++ UinProto2/registrationProcess.swift
@@ -11,6 +11,64 @@ class procress {
 
 
 }
+class homePage: UIViewController {
+    
+    @IBAction func facebookLogin(sender: AnyObject) {
+        var fbloginView:FBLoginView = FBLoginView(readPermissions: ["email", "public_profile"])
+        var permissions = ["public_profile", "email"]
+        PFFacebookUtils.logInWithPermissions(permissions, block: {
+            (user: PFUser!, error: NSError!) -> Void in
+            if error == nil {
+                if user == nil {
+                    NSLog("Uh oh. The user cancelled the Facebook login.")
+                    println(error)
+                  
+                    
+                } else if user.isNew {
+                    NSLog("User signed up and logged in through Facebook!")
+                    
+                    FBRequestConnection.startForMeWithCompletionHandler({
+                        connection, result, error in
+                        if error == nil {
+                            println(result)
+                            user["firstName"] = result["first_name"]
+                            user["lastName"] = result["last_name"]
+                            user.username = result["email"] as! String
+                            user.email = result["email"] as!String
+                            user["gender"] = result["gender"]
+                            user.saveInBackgroundWithBlock({
+                                (success:Bool, error:NSError!) -> Void in
+                                if error == nil {
+                                   println("The user is saved")
+                                }
+                                
+                            })
+                        }
+                        
+                        
+                        
+                    })
+                    
+                } else {
+                    
+                    NSLog("User is already signed in with us")
+             
+                    
+                }
+            }
+            
+        })
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
 class registrationProcess: UIViewController {
     
     @IBOutlet weak var emailSignUp: UITextField!
@@ -18,7 +76,8 @@ class registrationProcess: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+       
+      
         // Do any additional setup after loading the view.
     }
     
@@ -45,9 +104,13 @@ class registrationProcess: UIViewController {
     
     var characterSet:NSCharacterSet = NSCharacterSet(charactersInString: "@memphis.edu")
     @IBAction func nextView(sender: AnyObject) {
-        if ( PFUser.currentUser().email.rangeOfCharacterFromSet(characterSet) != nil) {
-            if PFUser.currentUser()["emailVerifed"] as! Bool == true {
+        //Sign user in
+        PFUser.logInWithUsername(PFUser.currentUser().username, password: "123Test")
+        if ( PFUser.currentUser().email.rangeOfString("@memphis.edu") != nil) {
+            if PFUser.currentUser()["emailVerified"]  as! Bool == true {
                 println("This guy is ready to procreed")
+            } else {
+                println("This guy email is not Verified")
             }
         } else {
             println("you do not have a memphis.edu")
@@ -65,7 +128,9 @@ class registrationProcess: UIViewController {
     
 }
 class basicSignUp: UIViewController {
+    @IBOutlet weak var fName: UITextField!
     
+    @IBOutlet weak var lName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -77,6 +142,19 @@ class basicSignUp: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func submitData(sender: AnyObject) {
+        var user = PFUser.currentUser()
+        user["firstName"] = fName.text.capitalizedString
+        user["lName"] = lName.text.capitalizedString
+        user.saveInBackgroundWithBlock({
+            (success:Bool, error:NSError!) -> Void in
+            if error == nil {
+                println("updated username fields")
+            } else {
+                println("Not sucessfuk")
+            }
+        })
+    }
     
     /*
     // MARK: - Navigation
@@ -89,12 +167,30 @@ class basicSignUp: UIViewController {
     */
     
 }
-class extraSignUp: UIViewController {
+class extraSignUp: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    @IBOutlet weak var gender: UISegmentedControl!
+    @IBOutlet weak var classifcation: UIPickerView!
+    
+    @IBOutlet weak var age: UITextField!
+    var classes = ["FreshMan", "Sophmore", "Junior","Senior", "Graduate"]
+    var userClass = (String)()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         // Do any additional setup after loading the view.
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return classes[row]
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return classes.count
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.userClass = classes[row]
     }
     
     override func didReceiveMemoryWarning() {
@@ -103,6 +199,35 @@ class extraSignUp: UIViewController {
     }
     
     
+    @IBAction func submitData(sender: AnyObject) {
+        var user = PFUser.currentUser()
+        if gender.selectedSegmentIndex == 1 {
+            user["gender"] = "male"
+        } else {
+            user["gender"] = "female"
+        }
+        if age.text.toInt() != nil {
+            user["gender"] = age.text.toInt()
+        } else {
+            println("This is not a real number")
+        }
+        if userClass.isEmpty {
+            user["classifcation"] = userClass
+        } else {
+            
+        }
+        user.saveInBackgroundWithBlock({
+            (success:Bool, error:NSError!) -> Void in
+            if error == nil {
+                println("user object was saved")
+            } else {
+                println("user object was not saved")
+            }
+        })
+        
+
+        
+    }
     /*
     // MARK: - Navigation
     
