416894cd0b64f5ba62a18620be7640ccccc2ae38
Kareem Dasilva <kareemdasilva@Kareems-MacBook-Pro.local>
2015-08-03 17:08:48 -0500

    Finish that up folks

--- UinProto2/OrginzationViewC.swift
+++ UinProto2/OrginzationViewC.swift
@@ -13,7 +13,7 @@ class OrginzationViewC: UIViewController {
     @IBOutlet weak var orgName: UILabel!
     @IBOutlet weak var orgDescription: UITextView!
     
-    
+    var id = (String)() // contains creation org object id
     override func viewDidLoad() {
         super.viewDidLoad()
             
@@ -37,9 +37,11 @@ class OrginzationViewC: UIViewController {
             Organization["description"] = orgDescription.text
             Organization["owner"] = PFUser.currentUser() as PFObject
            Organization["admins"] = adminArray
+           
             Organization.saveInBackgroundWithBlock({
                 (success:Bool, error:NSError!) -> Void in
                 if error == nil {
+                     self.id = Organization.objectId
                     //If it was successful
                     println("Good job you created uin")
                     //Saves the organization to the User
@@ -47,7 +49,8 @@ class OrginzationViewC: UIViewController {
                     PFUser.currentUser().saveInBackgroundWithBlock({
                         (succ:Bool, error:NSError!) -> Void in
                         if error == nil {
-                            self.performSegueWithIdentifier("orgPass", sender: self)
+                            println(self.id)
+                            self.performSegueWithIdentifier("OrgPush", sender: self)
                         } else {
                             println("It failed")
                         }
@@ -61,14 +64,40 @@ class OrginzationViewC: UIViewController {
     }
    
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        var orgPage:OrginazationPage = segue.destinationViewController as! OrginazationPage
+        orgPage.orgID = id
+        
     }
-    */
 
+
+}
+class OrginazationPage: UIViewController {
+    
+    @IBOutlet weak var orgName: UILabel!
+    
+    @IBOutlet weak var orgDesciption: UILabel!
+    
+    var orgID = (String)()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Querys for data about the ORganization
+        var query = PFQuery(className: "Organization")
+        query.getObjectInBackgroundWithId(orgID, block: {
+            (object:PFObject!, error:NSError!) -> Void in
+            if error == nil {
+                self.orgName.text = object["name"] as? String
+                self.orgDesciption.text = object["description"] as? String
+            }
+        })
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
